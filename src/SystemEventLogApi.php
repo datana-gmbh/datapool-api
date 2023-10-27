c14e3c26120f47a98275026d7bc4bed205f1513d
Oskar Stark <oskarstark@googlemail.com>
2023-10-27 13:13:54 +0200

    Use CPP

--- src/SystemEventLogApi.php
+++ src/SystemEventLogApi.php
@@ -22,13 +22,10 @@ use Psr\Log\NullLogger;
  */
 final class SystemEventLogApi implements SystemEventLogApiInterface
 {
-    private DatapoolClient $client;
-    private LoggerInterface $logger;
-
-    public function __construct(DatapoolClient $client, ?LoggerInterface $logger = null)
-    {
-        $this->client = $client;
-        $this->logger = $logger ?? new NullLogger();
+    public function __construct(
+        private DatapoolClient $client,
+        private LoggerInterface $logger = new NullLogger(),
+    ) {
     }
 
     public function log(string $key, string $info, \DateTimeInterface $timestamp, string $creator, ?array $context = null, ?string $ttl = null): bool
